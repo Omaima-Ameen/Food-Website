6354dafdbf9c89912728757ffd31dae4a03f44e8
Omaima-Ameen <156653672+Omaima-Ameen@users.noreply.github.com>
2024-04-07 20:15:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let menubar = document.querySelector("#menu-bar")
+let navbar = document.querySelector(".navbar")
+
+menubar.onclick = () =>{
+    menubar.classList.toggle("fa-times")
+    navbar.classList.toggle("active")
+}
+console.log("doggy")
